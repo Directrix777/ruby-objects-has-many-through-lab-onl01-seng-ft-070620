4336f31af934a280abbd12c42a6b947e2797903c
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-21 19:09:37 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -5,5 +5,12 @@ class Doctor
   attr_accessor :name
   @@all = []
 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def appointments
+    Appointment.all.select{|appt| appt.doctor == self}
 
 end
